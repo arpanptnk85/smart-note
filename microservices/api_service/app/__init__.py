37a6a13aa19577d0fc6d7422552aca2882dcc70c
Arpan Patnaik <arpan.patnaik85@gmail.com>
2024-09-11 18:16:02 +0530

    Update: microservices removed

--- microservices/api_service/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from . import gateway
-
-app = Flask(__name__) # Register the app
-
-app.register_blueprint(gateway.bp)
--- microservices/user_service/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from . import routes
-
-app = Flask(__name__)
-app.register_blueprint(routes.user_bp)
