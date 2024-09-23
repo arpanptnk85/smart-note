00024ed0c48c27fa73cadc1f0a9eca41efd08f14
Arpan Patnaik <arpan.patnaik85@gmail.com>
2024-09-23 22:33:59 +0530

    Update: componet dashboard added.

--- frontend/app/src/app/components/dashboard/dashboard.component.ts
+++ frontend/app/src/app/components/dashboard/dashboard.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared';
-import {MatSidenavModule} from '@angular/material/sidenav'; 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SharedModule, MatSidenavModule],
+  imports: [SharedModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-  showFiller = false;
+
 }
