9e4216b5fa0ff072af0f03f8b697b7fd68344450
Arpan Patnaik <arpan.patnaik85@gmail.com>
2024-10-02 21:28:05 +0530

    Update: frontend components added.

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -2,10 +2,19 @@ import { Routes } from '@angular/router';
 import { routeGuard } from './guards/route.guard';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { MainComponent } from './components/main/main.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'main', component: MainComponent, canActivate: [routeGuard], children: [
+        { path: 'notes', component: NotesComponent, canActivate: [routeGuard] },
+        { path: 'tasks', component: TasksComponent, canActivate: [routeGuard]  },
+        { path: 'todos', component: TodosComponent, canActivate: [routeGuard]  },
+    ] },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: '/login' },
 ];
